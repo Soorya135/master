4155eaa1b6394177f46dfc6cac6ab12a1d84d350
soorya <sarvan135@yahoo.com>
2022-06-10 14:00:20 +0530

    this is my first commit

---
+++ cypress/integration/1-getting-started/soorya.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("Suite level",function(){
+    it("First Test Case",function(){
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get(".search-keyword").type("ca")
+        cy.wait(2000)
+        cy.get(".product:visible").should('have.length',4)
+        cy.get(".products").find(".product").should('have.length',4)
+        cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click()
+    })
+})
